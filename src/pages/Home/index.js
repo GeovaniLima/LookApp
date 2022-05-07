ddc315261818da1683d2d587a6b8c5da1a89c555
GeovaniLima <geovani.lima.silva@hotmail.com>
2022-05-07 16:39:41 -0300

    Criando components estilizados e Pagina Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,21 +1,28 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+
+import { Box, Title, Text, Button, Spacer } from '../../components'
 
 const Home = () => {
   return (
-    <View style={styles.block}>
-      <Text>Home</Text>
-    </View>
+    <Box hasPadding align="center" background="dark">
+      
+      <Box justify="center" align="center" fluid>
+        <Title color="light" variant="big" bold>LOOKAPP</Title>
+        <Spacer />
+        <Text align="center" spacing="0px 40px">Stay on top fo the fashion world and buy your favorite looks.</Text>
+      </Box>
+
+      <Box fluid justify="flex-end" align="center">
+        <Button block>
+          <Text color="light">SigIn my account</Text>
+        </Button>
+        <Spacer size="20px" />
+        <Text underline color="light" onPress={() => alert('teste')}>Create new account</Text>
+        <Spacer size="70px" />
+      </Box>
+
+    </Box>
   );
 };
-
-const styles = StyleSheet.create({
-  block: {
-    backgroundColor: '#FFF',
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-})
 
 export default Home;
